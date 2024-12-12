0d8516040e67136257180ba8e6d020fb7b65c8bd
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2024-12-11 23:50:33 -0400

    credentials

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,7 +2,8 @@ import { cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { initialize } from "fireorm";
 
-const credencials = require("../credentials.json");
+const credencials = JSON.parse(process.env.CREDENTIALS || "");
+
 
 initializeApp({
     credential: cert(credencials),
