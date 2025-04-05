a1839c9baf1962192427aea8995798292e56e5aa
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2025-04-05 16:58:34 -0400

    menuMobileUserHome

--- frontend/src/pages/UserHome/index.tsx
+++ frontend/src/pages/UserHome/index.tsx
@@ -1,25 +1,27 @@
-import {useEffect} from "react";
 
-import { Layout, Menu, } from "antd";
+import { Layout, Menu, Button} from "antd";
 import {MenuInfo} from "rc-menu/lib/interface";
 
 import { menuItems } from "./menuItens.jsx";
 import {Outlet, useNavigate} from "react-router-dom";
-import { Container, SiderC } from "./styles.js";
+import {Container, DrawerC, SiderC} from "./styles.js";
 import {userContext} from "../../contexts/UserContext.tsx";
+
+import useBreakpoint from "antd/es/grid/hooks/useBreakpoint.js";
+import {MenuOutlined} from "@ant-design/icons";
+import {useState} from "react";
+
+import {mapDarkRoomThemeToStyled} from "../../styles/themes/roomThemeMapper.ts";
 
 export function UserHome() {
 	const { Content, Footer } = Layout;
-	const {signOut, user, isLoggedIn} = userContext()
+	const {signOut, user,} = userContext()
 
 	const navigate = useNavigate();
-
-	//
-	// useEffect(() => {
-	// 	if (!isLoggedIn) {
-	// 		navigate("/login");
-	// 	}
-	// }, [isLoggedIn, navigate]);
+	const screens = useBreakpoint();
+	const [menuOpen, setMenuOpen] = useState(false);
+	const mappedTheme = mapDarkRoomThemeToStyled();
+	
 
 	function handleMenuClick(click: MenuInfo) {
 		const { key } = click;
@@ -46,25 +48,77 @@ export function UserHome() {
 				minHeight: "100vh",
 			}}
 		>
-			<SiderC>
-				<div className="demo-logo-vertical">
-					<img
-						alt="logo"
-						src="/Logo%20Sound%20Share%20Sem%20fundo.png"
-						width="150"
+			{!screens.md && (
+				<Button
+					type="text"
+					icon={<MenuOutlined />}
+					onClick={() => setMenuOpen(true)}
+					style={{
+						position: "fixed",
+						top: 20,
+						left: 1,
+						zIndex: 10,
+						fontSize: "20px",
+					}}
+				/>
+			)}
+
+			{screens.md && (
+				<SiderC>
+					<div className="demo-logo-vertical">
+						<img
+							alt="logo"
+							src="/Logo%20Sound%20Share%20Sem%20fundo.png"
+							width="150"
+						/>
+					</div>
+					<div className="userInfo">
+						<b>Bem vindo {user && 'name' in user ? user.name : 'Usuário'}</b>
+					</div>
+					<Menu
+						theme="dark"
+						defaultSelectedKeys={["1"]}
+						mode="inline"
+						items={menuItems}
+						onClick={handleMenuClick}
 					/>
-				</div>
+				</SiderC>
+			)}
+
+			{/* Menu lateral para mobile */}
+
+			<DrawerC
+				placement="left"
+				open={menuOpen}
+				onClose={() => setMenuOpen(false)}
+				bodyStyle={{padding: 0, backgroundColor: mappedTheme.token.colorBgContainer}}
+				headerStyle={{backgroundColor: mappedTheme.token.colorBgContainer, borderBottom: "none"}}
+				closeIcon={<span style={{color: mappedTheme.token.colorPrimary}}>X</span>}
+				width={300}
+				extra={
+					<div className="demo-logo-vertical">
+						<img
+							alt="logo"
+							src="/Logo%20Sound%20Share%20Sem%20fundo.png"
+							width="150"
+						/>
+					</div>
+				}
+			>
 				<div className="userInfo">
-					  <b>Bem vindo {user && 'name' in user ? user.name : 'Usuário'}</b>
+					<b>Bem vindo {user && 'name' in user ? user.name : 'Usuário'}</b>
 				</div>
 				<Menu
+					className="menu"
 					theme="dark"
+					style={{color:"white"}}
 					defaultSelectedKeys={["1"]}
 					mode="inline"
 					items={menuItems}
 					onClick={handleMenuClick}
 				/>
-			</SiderC>
+			</DrawerC>
+
 			<Layout>
 				<Content
 					style={{
@@ -78,7 +132,7 @@ export function UserHome() {
 						}}
 					>
 						<Container>
-							<Outlet />
+							<Outlet/>
 						</Container>
 					</div>
 				</Content>
